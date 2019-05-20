ef4e5a3aba5a8edb4ee0fa64a8913a768552dd07
Anish Aggarwal <anish.5@hotmail.com>
2019-05-19 21:43:32 -0400

    motion and headset working

---
+++ Operation Aincrad/Assets/MixedRealityToolkit.Services/HSPositioning.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HSPositioning : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Start()
+    {
+
+        UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+        
+    }
+}
