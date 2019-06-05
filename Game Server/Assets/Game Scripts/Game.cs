7fd93e10dc08e3820cd6ec045218280dbee3bc4c
Noor-Nasri <noor.nasri@hotmail.com>
2019-06-05 14:20:43 -0500

    Created Dynamic game specifications

--- Game Server/Assets/Game Scripts/Game.cs
+++ Game Server/Assets/Game Scripts/Game.cs
@@ -52,10 +52,45 @@ public class Game : MonoBehaviour
     }
 
     // Start is called before the first frame update
+    public Dictionary<string, Dictionary<string, string>> equipments = new Dictionary<string, Dictionary<string, string>> { }; // {weapons : {name: text}, clothing: {}}
+    public Dictionary<string, Dictionary<string, string>> skills = new Dictionary<string, Dictionary<string, string>> { };
+    public Dictionary<string, Dictionary<string, string>> conditions = new Dictionary<string, Dictionary<string, string>> { };
+
+    private void LoadIn(Dictionary<string, Dictionary<string, string>> reference, string[] placements, string text) {
+        int currentIndex = 0;
+      
+        for (int i = 0; i < placements.Length; i++)
+        {
+            reference[placements[i]] = new Dictionary<string, string> { };
+            currentIndex = currentIndex + text.Substring(currentIndex).IndexOf("\n") + 1;
+
+            while (true)
+            {
+                string name = text.Substring(currentIndex, text.Substring(currentIndex).IndexOf(": "));
+                currentIndex += name.Length + 2;
+
+                string info = text.Substring(currentIndex, text.Substring(currentIndex).IndexOf("\n"));
+                currentIndex += info.Length + 1;
+
+                Debug.Log(placements[i] + ", " + name + " is " + info);
+                equipments[placements[i]][name] = info;
+
+                if (currentIndex + 3 >= text.Length || text.Substring(currentIndex + 2, 1).Equals("-")) // went through all the lines
+                {
+                    Debug.Log("Done");
+                    currentIndex += 3;
+                    break;
+                }
+            }
+        }
+    }
+
     void Start()
     {
         // load in all the asset information into a dictionary
-
+        LoadIn(equipments, new string[] { "Clothing", "Weapons", "Items" }, equipmentFile.text);
+        LoadIn(skills, new string[] { "Normal", "Attack", "Magic" }, skillFile.text);
+        LoadIn(conditions, new string[] { "All" }, conditionList.text);
 
         // start the eternal game loop
         (new Thread(GameLoop)).Start();
