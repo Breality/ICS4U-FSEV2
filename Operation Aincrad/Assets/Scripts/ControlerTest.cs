ce22c44b4b60c3bd42583b5a6c2935cda1a93733
fairnightzz <zhehaizhang1@gmail.com>
2019-06-08 11:03:12 -0400

    Swords refined, put collider on Orochimaru only

---
+++ Operation Aincrad/Assets/Scripts/ControlerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string[] names = Input.GetJoystickNames();
+        Debug.Log("Connected Joysticks:");
+        foreach (string stick in names)
+        {
+            Debug.Log("Joystick " + stick);
+        }
+    }
+}
