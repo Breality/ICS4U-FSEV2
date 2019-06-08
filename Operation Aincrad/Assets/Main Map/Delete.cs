5a2d19286a7d93c8ff4a1954974e34bcd6ef06d1
fairnightzz <zhehaizhang1@gmail.com>
2019-06-08 16:37:51 -0400

    heresur pushes u prick

---
+++ Operation Aincrad/Assets/Main Map/Delete.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Delete : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int i = 0;
+        foreach (Transform child in this.transform)
+        {
+            if (i%2 == 0)
+            {
+                Destroy(child.gameObject);
+                
+            }
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
