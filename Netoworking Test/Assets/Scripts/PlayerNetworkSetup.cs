5eaf3f56f8fa270f17537b32bf1f9e448ae53d61
fairnightzz <zhehaizhang1@gmail.com>
2019-06-11 17:56:41 -0400

    working

---
+++ Netoworking Test/Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class PlayerNetworkSetup : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        Renderer[] rends = GetComponentsInChildren<Renderer>();
+        foreach(Renderer ren in rends)
+        {
+            ren.enabled = false;
+        }
+
+        GetComponent<NetworkAnimator>().
+    }
+}
