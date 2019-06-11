b08e8cf4a536bc6d08eb6d39bac18e336c8f9e51
fairnightzz <zhehaizhang1@gmail.com>
2019-06-11 17:12:23 -0400

    back in time

--- Netoworking Test/Assets/uMMORPG/Scripts/NetworkStartPositionForClass.cs
+++
@@ -1,7 +0,0 @@
-﻿// Simple script that inherits from NetworkStartPosition to make class based
-// spawns.
-using UnityEngine.Networking;
-
-public class NetworkStartPositionForClass : NetworkStartPosition {
-    public Player playerPrefab;
-}
