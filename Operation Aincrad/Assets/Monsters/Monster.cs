d48cab7ecd4bfdb66565e3d57d9e834faa7616dc
fairnightzz <zhehaizhang1@gmail.com>
2019-06-13 11:35:38 -0400

    Started monsters

---
+++ Operation Aincrad/Assets/Monsters/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public string type;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(type == "skeleton")
+        {
+            Debug.Log("move this shit");
+        }
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Operation Aincrad/Assets/Monsters/Spawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public int level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(level == 0)
+        {
+            //Copy and instantiate new monster
+        }
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
