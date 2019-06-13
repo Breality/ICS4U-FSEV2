dd530ca848a1b82de5a9024ffd8975e59ed16d68
fairnightzz <zhehaizhang1@gmail.com>
2019-06-13 17:35:59 -0400

    Monsters

--- Operation Aincrad/Assets/Monsters/Spawn.cs
+++ Operation Aincrad/Assets/Monsters/Spawn.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
+    public Transform parent;
     public int level;
     // Start is called before the first frame update
+    private List<GameObject> monsters = new List<GameObject>();
+
     void Start()
     {
+        foreach(Transform child in parent)
+        {
+            monsters.Add(child.gameObject);
+        }
+
         if(level == 0)
         {
             //Copy and instantiate new monster
+            GameObject m = Instantiate(monsters[0],parent);
+            m.transform.localPosition = new Vector3(0, 0, 0);
+            m.SetActive(true);
+            
+
+
         }
     }
 
