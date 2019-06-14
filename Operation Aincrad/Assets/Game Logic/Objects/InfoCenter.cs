83cb4304815521d575245a08f5e2ad771c2296ca
fairnightzz <zhehaizhang1@gmail.com>
2019-06-14 13:34:11 -0400

    Body added

--- Operation Aincrad/Assets/Game Logic/Objects/InfoCenter.cs
+++ Operation Aincrad/Assets/Game Logic/Objects/InfoCenter.cs
@@ -18,7 +18,8 @@ public class InfoCenter : MonoBehaviour // local data held to make life easier, 
         { "Curved Sword", 75 }, {"Dark Sword", 75 }, {"Dothraki Sword", 50 }, {"Elucidator", 100 }, {"Long Sword", 150 }, {"Shark Staff", 200 },
         { "High Boots"  , 125}, {"White Shoes",30 }, {"Converse"      , 45 }, {"Army Boots", 55  }, {"Aviator's Hat",30 },{"Flower Hat" , 35  },
         { "Loki Helmet" , 100}, {"Soldier Helmet", 20 }, {"Sorcery Helmet", 60 }, {"Spartan Helmet", 45 }, {"Templar Helmet", 45 },
-        { "Top Hat" , 35 }, {"Viking Helmet", 45}
+        { "Top Hat" , 35 }, {"Viking Helmet", 45} , {"Knight Armour", 50 }, {"Aqua Dress", 40}, {"Black Dress", 45},
+        {"Purple Dress", 35}, {"Red Dress", 60}, {"White Dress", 65}, {"Red Jacket", 25}
     };
 
     // basic info
