261a78589cc4db72c8fbb036afbdd0c6f2b184d1
Anish Aggarwal <anish.5@hotmail.com>
2019-06-17 12:21:28 -0400

    FIXING MUSIC

---
+++ Operation Aincrad/Assets/Music/MusicScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    [SerializeField]
+    private AudioListener menuListen, gameListen;
+    [SerializeField]
+    private AudioSource menuPlayer, gamePlayer;
+    private void OnDisable()
+    {
+        menuListen.enabled = false;
+        menuPlayer.enabled = false;
+        gameListen.enabled = true;
+        gamePlayer.enabled = true;
+    }
+}
